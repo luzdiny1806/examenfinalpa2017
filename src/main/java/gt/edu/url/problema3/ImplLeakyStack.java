3c0882b79e4bbab52a6ab3b9d2971f1261769b28
Luz Rodriguez <31348540+luzdiny1806@users.noreply.github.com>
2017-11-22 08:13:02 -0600

    Subiendo el Proyecto a las 8:12.

---
+++ src/main/java/gt/edu/url/problema3/ImplLeakyStack.java
@@ -0,0 +1,96 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gt.edu.url.problema3;
+
+/**
+ *
+ * @author luzdy
+ */
+public class ImplLeakyStack<E> implements LeakyStack{
+
+    private static class Node<E>{
+        private E element;
+		private Node<E> next;
+		public Node(E element, Node<E> next) {
+			super();
+			this.element = element;
+			this.next = next;
+		}
+		public E getElement() {
+			return element;
+		}
+		public Node<E> getNext() {
+			return next;
+		}
+		public void setNext(Node<E> next) {
+			this.next = next;
+		}
+    }
+    
+    private Node<E> tail = null;
+	
+	private int size = 0;
+	
+	public int size() {return size;}
+	
+	public boolean isEmpty() { return size == 0;}
+	
+	public E first() {
+		if (isEmpty()) return null;
+		return tail.getNext().getElement();//Primer cambio
+	}
+	
+	public E last() {
+		if (isEmpty()) return null;
+		return tail.getElement();
+	}
+	
+	public void rotate() {
+		if (tail != null)
+			tail = tail.getNext();
+	}
+	
+	public void addFirst(E e) {
+		
+		if(size == 0) {
+			tail = new Node<>(e, null);
+			tail.setNext(tail);
+		}else {
+			Node<E> newest = new Node<>(e, tail.getNext());
+			tail.setNext(newest);
+		}
+		size++;
+	}
+	
+	public void addLast(E e) {
+		addFirst(e);
+		tail = tail.getNext();
+	}
+	
+	public E removeFirst() {
+		if(isEmpty()) return null;
+		Node<E> head = tail.getNext();
+		if(head == tail) tail = null;
+		else tail.setNext(head.getNext());
+		size--;
+		return head.getElement();
+	}
+        
+            @Override
+    public Object saveHistory(Object e) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Object actual() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Object undo() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+}
